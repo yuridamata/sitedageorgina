4a9aa58d960bd1a8ef88c68f13c2ca875d05a552
Yuri da Mata <yuridamata@gmail.com>
2022-05-21 16:52:25 -0300

    feat(Loading Page): loading page
    Loading page included

--- components/Loading/index.js
+++ components/Loading/index.js
@@ -1,9 +1,9 @@
 import styles from "./styles.module.scss";
 import Image from "next/image";
 
-const Loading = () => {
+const Loading = ({ loading }) => {
   return (
-    <div className={styles.loadingWrapper}>
+    <div className={loading === true ? styles.loadingWrapper : styles.loadedWrapper}>
       <img src={"/logoGeorgina.png"} className={styles.logo} />
     </div>
   );
