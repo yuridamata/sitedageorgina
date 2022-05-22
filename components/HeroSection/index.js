f2e1816ca37c48ecd6ace816fec6cdee1571c25a
Yuri da Mata <yuridamata@gmail.com>
2022-05-21 21:42:59 -0300

    feat(HeroSection): construção da hero section
    Versão inicial da hero section

--- components/HeroSection/index.js
+++ components/HeroSection/index.js
@@ -1,7 +1,24 @@
 import styles from "./styles.module.scss";
+import Logo from "../../public/logoGeorgina.png";
+import Image from "next/image";
 
-const HeroSection = () => {
-  return <div className={styles.heroSection}></div>;
+const HeroSection = ({ heroHeight }) => {
+  return (
+    // minHeight is defined here because it's necessary on Menu behaviour
+    <div className={styles.heroSection} style={{ minHeight: heroHeight }}>
+      <div className={styles.content}>
+        <div>
+          <Image src={"/logoGeorgina.png"} width={654} height={340} />
+        </div>
+      </div>
+      <div className={styles.callToAction}>
+        <a>
+          <p>Nos conheça</p>
+          <p className={styles.arrow}> &#x27A4; </p>
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default HeroSection;
